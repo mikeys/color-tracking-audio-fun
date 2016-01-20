8e276c17fd75975b502910c35b750996e0dedbc5
Miki Shiran <mikeyshiran@gmail.com>
2016-01-20 13:03:51 +0200

    Added movement detector specific stuff

--- examples/assets/color-audio-tracker.js
+++ examples/assets/color-audio-tracker.js
@@ -18,13 +18,23 @@ ColorAudioTracker = function(options) {
       'magenta': 'drums'
     },
     recognizer: new DollarRecognizer(),
-  }
+    movementDetectorConfig: {
+      numberOfConsequentPoints: 30,
+      velocityThreshold: 0.1, // px/ms
+      timeThreshold: 2000 // ms
+    }
+  };
+
+  console.log(defaultOptions);
 
   defaultOptions.extendOwn(options);
   options = defaultOptions;
+  console.log(options);
 
-  this.colors = Object.keys(options.colorInstrument);
-  this.tracker = new tracking.ColorTracker(that.colors);
+  this.colorInstrument = options.colorInstrument;
+  this.colors = function() { return Object.keys(that.colorInstrument); };
+  this.tracker = new tracking.ColorTracker(that.colors());
+  this.colorMovementDetector = initializeMovementDetectors();
   this.recognizer = options.recognizer;
   this.videoElemId = options.videoElemId;
   this.velocityThreshold = options.velocityThreshold;
@@ -33,7 +43,20 @@ ColorAudioTracker = function(options) {
   this.canvas = document.getElementById('canvas');
   this.context = that.canvas.getContext('2d');
 
-  this.track = function(event) {
+  function initializeMovementDetectors() {
+    var map = {};
+    for (var color in that.colors()) {
+      map[color] = new MovementDetector(that.movementDetectorConfig, onMovementStop);
+    }
+
+    return map;
+  };
+
+  function onMovementStop(points) {
+    console.log(that.recognizer.Recognize(points));
+  }
+
+  function track(event) {
     if (event.data.length === 0) {
       // No colors were detected in this frame.
     } else {
@@ -46,11 +69,13 @@ ColorAudioTracker = function(options) {
 
         context.rect(rect.x, rect.y, rect.width, rect.height);
         console.log(rect);
-
-        var points = [new Point(0,0), new Point(0, 50), new Point(50, 0), new Point(50, 50), new Point(0,0)]
-        console.log(that.recognizer.Recognize(points));
+        // var points = [new Point(0,0), new Point(0, 50), new Point(50, 0), new Point(50, 50), new Point(0,0)]
+        // console.log(that.recognizer.Recognize(points));
 
         var centerPoint = Point.centerOf(rect);
+        that.colorMovementDetector[rect.color].recordMeasurement(
+          centerPoint, Date.now());
+
         context.fillRect(centerPoint.x, centerPoint.y, 1, 1);
         console.log(centerPoint);
 
@@ -58,10 +83,10 @@ ColorAudioTracker = function(options) {
         context.closePath();
       });
     }
-  };
+  }
 
   // Register tracker
-  this.tracker.on('track', that.track);
+  this.tracker.on('track', track);
 
   this.init = function() {
     tracking.track(that.videoElemId, that.tracker, { camera: true });
